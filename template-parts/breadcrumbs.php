5987629c32a46778fa3e9f9585f2523ab4327690
rinasuzuki52 <rinasuzuki1015@gmail.com>
2025-11-28 14:32:43 +0900

    ページネーション修正

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -1,5 +1,5 @@
- <div class="p-blog-list__breadcrumbs p-blog-list-breadcrumbs p-breadcrumbs">
-    <div class="p-blog-list-breadcrumbs__inner p-breadcrumbs__inner l-inner">
+ <div class="p-breadcrumbs">
+    <div class="l-inner">
         <nav class="p-breadcrumbs__list">
             <ul>
                 <?php if (function_exists('bcn_display')) {
