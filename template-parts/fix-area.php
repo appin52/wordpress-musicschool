c6e7b311f55951fa871ce290a1831a10db6e0dd3
rinasuzuki52 <rinasuzuki1015@gmail.com>
2025-12-11 08:46:36 +0900

    お問い合わせボタン修正

--- template-parts/fix-area.php
+++ template-parts/fix-area.php
@@ -1,3 +1,19 @@
+<div class="p-fix-area">
+      <div class="p-fix-area__inner">
+         <a href="#" class="c-back-btn">
+            <div class="c-back-btn__icon">
+               <img srcset="<?php echo get_template_directory_uri(); ?>/images/top-back-btn.svg">
+            </div>
+         </a>
+         <?php if ( !is_page('contact') ) : ?>
+            <a href="<?php echo esc_url(home_url('contact')); ?>" class="c-btn c-btn--contact js-contact-btn">
+               お問い合わせ
+            </a>
+         <?php endif; ?>
+      </div>
+</div>
+
+
 <!-- トップへ戻るボタン -->
  <a href="#" class="c-back-btn">
     <div class="c-back-btn__icon">
