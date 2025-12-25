36d8cf90d5d7d9b5988862b92207a2896de8f938
rinasuzuki52 <rinasuzuki1015@gmail.com>
2025-12-25 16:01:00 +0900

    修正

--- template-parts/fix-area.php
+++ template-parts/fix-area.php
@@ -2,11 +2,11 @@
       <div class="p-fix-area__inner">
          <a href="#" class="c-back-btn">
             <div class="c-back-btn__img">
-               <img srcset="<?php echo get_template_directory_uri(); ?>/images/top-back-btn.svg">
+               <img srcset="<?php echo get_template_directory_uri(); ?>/images/top-back-btn.svg" alt="">
             </div>
          </a>
          <?php if ( !is_page('contact') ) : ?>
-            <a href="<?php echo esc_url(home_url('contact')); ?>" class="c-btn c-btn--contact" alt="">
+            <a href="<?php echo esc_url(home_url('contact')); ?>" class="c-btn c-btn--contact">
                お問い合わせ
             </a>
          <?php endif; ?>
